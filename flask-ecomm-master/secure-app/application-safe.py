680de54243f9ab81149e856e4617f0240dc4903c
Dylan818 <limdylan818@gmail.com>
2022-08-16 19:15:28 +0800

    update 11: preparing to host

--- flask-ecomm-master/secure-app/application-safe.py
+++ flask-ecomm-master/secure-app/application-safe.py
@@ -233,15 +233,12 @@ def validate_username(user_input):
     pattern = r"\d|[a-z]|[A-Z]"
     if re.search(pattern, user_input):
         x = re.findall(pattern, user_input)
-        print(x)
         if len(x) == len(user_input):
-            print("h1")
             return True
         else:
             return False
 
     else:
-        print("Not found")
         return False
 
 
